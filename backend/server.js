9ec65a843047a4a427a1f5d912e056ccb95c0707
Mark <muchirimark2@gmail.com>
2022-12-01 17:38:58 +0300

    initialized

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+// process.env.PORT   ---- Allows us to access the .env port. If it's not found itll use port 5000
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
