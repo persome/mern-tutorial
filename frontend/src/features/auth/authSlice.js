7825d18bb68ca267105c4585fe87f18c175ffbf5
Mark <muchirimark2@gmail.com>
2022-12-22 03:04:25 +0300

    - install frontend dependencies: axios, react-toastify
    On branch learn
        Your branch is ahead of 'origin/learn' by 1 commit.
        (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   frontend/src/app/store.js
    	new file:   frontend/src/features/auth/authSlice.js

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+//get user from localStorage
+const user = JSON.parse(localStorage.getItem('user'))
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+}
+
+//register user
+
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false
+            state.isSuccess = false
+            state.isError = false
+            state.message = ''
+        }
+    },
+    extrareducers: () => { }
+})
+
+export const { reset } = authSlice.actions
+export default authSlice.reducer
